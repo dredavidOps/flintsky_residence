e8052ffb0b5060090c8c38ac91a4fdba4271154e
David Yaotse <drewizzly@gmail.com>
2025-07-18 23:08:04 +0200

    API Monitoring

--- backend/core/urls.py
+++ backend/core/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from django.http import JsonResponse
+from django_prometheus import exports
 from django.urls import path, include
+from django.http import JsonResponse
 
 urlpatterns = [
     path('', lambda request: JsonResponse({"message":"Flintsky Backend is running"})),
+    path('metrics/', exports.ExportToDjangoView),
     path("admin/", admin.site.urls),
     path('api/', include('booking.urls')),
 ]
